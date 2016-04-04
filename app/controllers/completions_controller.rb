d837f76207cc9be7f8d869c2c52c4eb04158e892
Octavian Rotari <octavianrotari@gmail.com>
2016-04-04 22:39:57 +0200

    complete a todos belongs to the todo model

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -1,12 +1,13 @@
 class CompletionsController < ApplicationController
 
   def create
-    todo.touch :completed_at
+    todo.complete!
     redirect_to todos_path
   end
 
-  private 
-  def todo 
+  private
+  def todo
     current_user.todos.find(params[:todo_id])
   end
+
 end
